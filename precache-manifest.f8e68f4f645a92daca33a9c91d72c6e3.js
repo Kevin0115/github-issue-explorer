3895a641e7be2a8a721d6f5b290e9560a28ce389
Kevin0115 <kevin.choi0115@gmail.com>
2019-01-23 22:41:56 -0800

    Updates

---
+++ precache-manifest.f8e68f4f645a92daca33a9c91d72c6e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "49c092514c29c82004792213332c11ee",
+    "url": "/github-issue-explorer/static/media/close.49c09251.svg"
+  },
+  {
+    "revision": "521a79030910c7d7ebfa",
+    "url": "/github-issue-explorer/static/css/main.dd933a19.chunk.css"
+  },
+  {
+    "revision": "4eaa2fb5bf664cc496fc",
+    "url": "/github-issue-explorer/static/js/1.4eaa2fb5.chunk.js"
+  },
+  {
+    "revision": "0e65a55c9a5f58476ab5",
+    "url": "/github-issue-explorer/static/js/runtime~main.0e65a55c.js"
+  },
+  {
+    "revision": "4d4782aeaf28efdf222a2b82c8b73283",
+    "url": "/github-issue-explorer/static/media/search.4d4782ae.svg"
+  },
+  {
+    "revision": "2d297b28958f1b0add686aa0d8fc760b",
+    "url": "/github-issue-explorer/static/media/circle.2d297b28.svg"
+  },
+  {
+    "revision": "f7396a8d8ececa8b625f9eba428288fe",
+    "url": "/github-issue-explorer/static/media/pull-request.f7396a8d.svg"
+  },
+  {
+    "revision": "29eed3f5d6b808b475e2a5a51b62d65a",
+    "url": "/github-issue-explorer/static/media/issue-closed.29eed3f5.svg"
+  },
+  {
+    "revision": "521a79030910c7d7ebfa",
+    "url": "/github-issue-explorer/static/js/main.521a7903.chunk.js"
+  },
+  {
+    "revision": "3fb4d2902124120fbdc713a267b36403",
+    "url": "/github-issue-explorer/static/media/montserrat-regular.3fb4d290.woff"
+  },
+  {
+    "revision": "b94d4bb6c9c96d53b7a724f6d7397b49",
+    "url": "/github-issue-explorer/static/media/montserrat-regular.b94d4bb6.woff2"
+  },
+  {
+    "revision": "b760b19f49a84fd4b3ce2252969dec30",
+    "url": "/github-issue-explorer/static/media/montserrat-bold.b760b19f.woff"
+  },
+  {
+    "revision": "45af372786e4fc6d6c54a88018cd6ce5",
+    "url": "/github-issue-explorer/static/media/montserrat-bold.45af3727.woff2"
+  },
+  {
+    "revision": "a820606bee2646d5ec81751628904dd4",
+    "url": "/github-issue-explorer/static/media/montserrat-light.a820606b.woff"
+  },
+  {
+    "revision": "d0d8af75aedd27ba147c6b6ad7d2eba7",
+    "url": "/github-issue-explorer/static/media/montserrat-light.d0d8af75.woff2"
+  },
+  {
+    "revision": "2577aa00775038ad90c3f2e608cf9115",
+    "url": "/github-issue-explorer/index.html"
+  }
+];
